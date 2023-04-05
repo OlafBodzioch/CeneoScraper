6819ea90961aa060bc6e761ac3d14162a2a9efcf
OlafBodzioch <226356@student.uek.krakow.pl>
2023-04-05 13:16:24 +0200

    2023-04-05 OB

--- scraper.py
+++ scraper.py
@@ -1,10 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
-#product_code = input("Podaj kod produktu: ")
-product_code = 97384644
-url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
-response = requests.get(url, auth=('user', 'pass'))
-page_dom = BeautifulSoup(response.text, "html.parser")
+def get_cos(ancestor, selector = None, attribute = None, return_list = False):
+    try:
+        if return_list:
+            return [tag.text.strip() for tag in ancestor.select(selector)].copy()
+        if not selector:
+            return ancestor[attribute]
+        if attribute:
+            return ancestor.select_one(selector)[attribute].strip()
+        return ancestor.select_one(selector).text.strip()
+    except AttributeError:
+        return None
 
-print(page_dom.prettify)
+#product_code = input("Podaj kod produktu: ")
+product_code = "97384644"
+url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+response = requests.get(url)
+page = BeautifulSoup(response.text, "html.parser")
+opinions = page.select("div.js_product-review")
+all_opinions = []
+
+for opinion in opinions:
+    single_opinion = {
+        "opinion-id":  opinion["data-entry-id"],
+        "author": opinion.select_one("span.user-post__author-name").text.strip(),
+        "recommendation": opinion.select_one("span.user-post__author-recommendation").text.strip(),
+        "score": opinion.select_one("span.user-post__score-count").text.strip(),
+        "purchased": opinion.select_one("div.review-pz").text.strip(),
+        "published_at": opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"],
+        "purchased_at": opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"],                                                                                             
+        "thumbs_up": opinion.select_one("button.vote-yes > span").text.strip(),
+        "thumbs_down": opinion.select_one("button.vote-no > span").text.strip(),
+        "content": opinion.select_one("div.user-post__text").text.strip(),
+        "pros": [pros.text.strip() for pros in opinion.select("div.review-feature__col:has(> div. review-feature__title review-feature__title--positives) > div.review-feature__item")],
+        "cons": [cons.text.strip() for cons in opinion.select("div.review-feature__col:has(> div. review-feature__title review-feature__title--negatives) > div.review-feature__item")],
+    }
+    all_opinions.append(single_opinion)
+print(all_opinions)
